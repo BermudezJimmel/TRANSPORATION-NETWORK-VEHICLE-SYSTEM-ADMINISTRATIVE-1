803dc46a956867ba1ab332819b67e24398144bfc
Jimmel Bermudez <70561959+BermudezJimmel@users.noreply.github.com>
2020-10-29 12:04:02 +0800

    first commit

---
+++ src/databaseconn.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author jimmelbermudez7
+ */
+public class databaseconn {
+    Connection conn=null;
+    public static Connection ConnecrDb(){
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn;
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/3206GrabDb","root","");
+             
+            return conn;
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null,e);
+            return null;
+        }
+    }
+    
+}
